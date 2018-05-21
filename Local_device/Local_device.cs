0ffc31059fa1a4a136c946ed46f7689fdb019b95
ASUS <ASUS@DESKTOP-SL1QFU2>
2018-05-21 10:37:46 +0200

    aa

---
+++ Local_device/Local_device.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logger;
+
+namespace Local_device
+{
+    public class Local_device
+    {
+        public int Code { get; set; }
+
+        public TimeSpan vrijeme { get; set; }
+        public int Value { get; set; }
+        public static Random r = new Random();
+        Logger.Logger logger = new Logger.Logger();
+
+        public void SendToBuffer()
+        {
+            //da li ovdje idu vrijednosti koje pokupimo?
+            int code = r.Next(1, 9);
+            float value = r.Next();
+            logger.WriteLogg(String.Format("Writter sending to Buffer code {0} and value {1}", code, value));
+            buffer.SendData(code, value);
+
+        }
+    }
+}
---
+++ Logger/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class Logger
+    {
+        public Logger()
+        {
+
+        }
+        public void WriteLogg(string message)
+        {
+            TextWriter tw = new StreamWriter(@"logger.txt", true);
+            tw.WriteLine(String.Format("{0}: {1}", DateTime.Now, message));
+            tw.Close();
+        }
+    }
+}
